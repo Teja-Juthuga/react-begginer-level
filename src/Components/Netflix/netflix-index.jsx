98a60ab6fed83995d8631f8a29522a84e50a073a
Teja Juthuga <tejajuthuga@gmail.com>
2024-03-13 23:12:27 +0530

    developed Netflix main

--- src/Components/Netflix/netflix-index.jsx
+++ src/Components/Netflix/netflix-index.jsx
@@ -1,12 +1,15 @@
 import Background from './assets/netflixBanner.jpg';
 import { NetflixHeader } from './netflix-header';
 
+import { NetflixMain } from './netflix-main';
+
 
 export function NetflixIndex(){
     return (
         <div style={{backgroundImage: `url(${Background})` , height:"100vh"}}>
             <div style={{height:"100vh", backgroundColor:"rgba(0,0,0,0.5)"}}>
                 <NetflixHeader />
+                <NetflixMain />
             </div>
         </div>
     )
---
+++ src/Components/Netflix/netflix-main.jsx
@@ -0,0 +1,8 @@
+export function NetflixMain(){
+    return(
+        <div className="text-light text-center mt-5">
+            <h1> Unlimited movies, TV shows and more </h1>
+            <h4> Starts at ₹149. Cancel anytime. </h4>
+        </div>
+    )
+}
